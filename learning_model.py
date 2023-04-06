30abc616a4b4431ded9272dba8387ed48662a6a3
Geani Pocroianu <pocroianu.nicolae@isoftbet.com>
2023-04-06 20:31:02 +0300

    trading bot using alpaca and alpha_vantage *cleaning

--- learning_model.py
+++ learning_model.py
@@ -18,8 +18,8 @@ alpha_vantage_api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
 # Fetch historical data for a stock
 def fetch_data(symbol, start, end):
     try:
-        ts = TimeSeries(key=alpha_vantage_api_key, output_format='pandas')
-        df, _ = ts.get_daily_adjusted(symbol, outputsize='full')
+        ts = TimeSeries(key=alpha_vantage_api_key, output_format="pandas")
+        df, _ = ts.get_daily_adjusted(symbol, outputsize="full")
 
         # Convert the date strings to datetime objects, sort the index, and filter the date range
         df.index = pd.to_datetime(df.index)
@@ -35,67 +35,6 @@ def fetch_data(symbol, start, end):
         return None
 
 
-# Preprocess the data and create features
-def preprocess_data(df):
-    # Create a new DataFrame to avoid SettingWithCopyWarning
-    df_processed = df.copy()
-    
-    # Calculate moving averages
-    df_processed['SMA_10'] = talib.SMA(df_processed['4. close'], timeperiod=10)
-    df_processed['SMA_20'] = talib.SMA(df_processed['4. close'], timeperiod=20)
-
-    # Calculate Bollinger Bands
-    df_processed['upper_BB'], df_processed['middle_BB'], df_processed['lower_BB'] = talib.BBANDS(df_processed['4. close'], timeperiod=20)
-
-    # Calculate RSI
-    df_processed['RSI'] = talib.RSI(df_processed['4. close'], timeperiod=14)
-
-    # Calculate MACD
-    df_processed['MACD'], df_processed['MACD_signal'], _ = talib.MACD(df_processed['4. close'], fastperiod=12, slowperiod=26, signalperiod=9)
-
-    # Drop missing values
-    df_processed = df_processed.dropna()
-
-    # Define the target variable (e.g., next day's close)
-    df_processed['target'] = df_processed['4. close'].shift(-1)
-
-    # Drop the last row, which will have a missing target value
-    df_processed = df_processed[:-1]
-
-    return df_processed
-
-
-# Train and evaluate a machine learning model
-def train_model(df):
-    features = df.columns.drop(['target']).tolist()
-    target = 'target'
-
-    X = df[features]
-    y = df[target]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    model = GradientBoostingRegressor(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    print(f'Mean Squared Error: {mse}')
-
-    # Plot the stock data with the predicted values
-    plt.figure(figsize=(14, 8))
-    plt.plot(df.index[-len(y_test):], y_test, label='Actual Prices', color='blue')
-    plt.plot(df.index[-len(y_test):], y_pred, label='Predicted Prices', color='red')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('Stock Price Prediction')
-    plt.legend()
-    # plt.show()
-
-    return model, features
-
-
-
 # Fetch historical data for multiple stocks
 def fetch_data_multiple(symbols, start, end):
     data = {}
@@ -105,12 +44,81 @@ def fetch_data_multiple(symbols, start, end):
             data[symbol] = df
     return data
 
+
+# Preprocess the data and create features
+def preprocess_data(df):
+    # Create a new DataFrame to avoid SettingWithCopyWarning
+    df_processed = df.copy()
+
+    # Calculate moving averages
+    df_processed["SMA_10"] = talib.SMA(df_processed["4. close"], timeperiod=10)
+    df_processed["SMA_20"] = talib.SMA(df_processed["4. close"], timeperiod=20)
+
+    # Calculate Bollinger Bands
+    (
+        df_processed["upper_BB"],
+        df_processed["middle_BB"],
+        df_processed["lower_BB"],
+    ) = talib.BBANDS(df_processed["4. close"], timeperiod=20)
+
+    # Calculate RSI
+    df_processed["RSI"] = talib.RSI(df_processed["4. close"], timeperiod=14)
+
+    # Calculate MACD
+    df_processed["MACD"], df_processed["MACD_signal"], _ = talib.MACD(
+        df_processed["4. close"], fastperiod=12, slowperiod=26, signalperiod=9
+    )
+
+    # Drop missing values
+    df_processed = df_processed.dropna()
+
+    # Define the target variable (e.g., next day's close)
+    df_processed["target"] = df_processed["4. close"].shift(-1)
+
+    # Drop the last row, which will have a missing target value
+    df_processed = df_processed[:-1]
+
+    return df_processed
+
+
+# Train and evaluate a machine learning model
+def train_model(df):
+    features = df.columns.drop(["target"]).tolist()
+    target = "target"
+
+    X = df[features]
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = GradientBoostingRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Mean Squared Error: {mse}")
+
+    # Plot the stock data with the predicted values
+    plt.figure(figsize=(14, 8))
+    plt.plot(df.index[-len(y_test) :], y_test, label="Actual Prices", color="blue")
+    plt.plot(df.index[-len(y_test) :], y_pred, label="Predicted Prices", color="red")
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.title("Stock Price Prediction")
+    plt.legend()
+    # plt.show()
+
+    return model, features
+
+
 # Train and evaluate a machine learning model for multiple stocks
 def train_models(data):
     models = {}
     feature_names = {}
     for symbol, df in data.items():
-        print(f'Training model for {symbol}')
+        print(f"Training model for {symbol}")
         processed_data = preprocess_data(df)
         model, features = train_model(processed_data)
         models[symbol] = model
--- main.py
+++ main.py
@@ -9,47 +9,24 @@ load_dotenv()
 
 ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
 ALPACA_API_SECRET = os.getenv("ALPACA_API_SECRET")
-BASE_URL = 'https://paper-api.alpaca.markets'  # Use paper trading for testing
+BASE_URL = "https://paper-api.alpaca.markets"  # Use paper trading for testing
+
+
+QUANTITY = 5
 
 # Connect to the API
-api = tradeapi.REST(ALPACA_API_KEY, ALPACA_API_SECRET, BASE_URL, api_version='v2')
+api = tradeapi.REST(ALPACA_API_KEY, ALPACA_API_SECRET, BASE_URL, api_version="v2")
+
 
 # Define your trading strategy
 def should_buy(symbol):
     # Implement your buy logic here
     return True
 
+
 def should_sell(symbol):
     # Implement your sell logic here
     return False
-
-# Execute trades based on your strategy
-def execute_trades():
-    # Get a list of your positions
-    positions = api.list_positions()
-
-    # Check if we should sell any positions
-    for position in positions:
-        if should_sell(position.symbol):
-            api.submit_order(
-                symbol=position.symbol,
-                qty=position.qty,
-                side='sell',
-                type='market',
-                time_in_force='gtc'
-            )
-
-    # Check if we should buy any new stocks
-    symbols = ['AAPL', 'GOOG', 'MSFT']  # Replace with your desired symbols
-    for symbol in symbols:
-        if should_buy(symbol):
-            api.submit_order(
-                symbol=symbol,
-                qty=1,  # Replace with your desired quantity
-                side='buy',
-                type='market',
-                time_in_force='gtc'
-            )
 
 
 def generate_signals(models, feature_names, data):
@@ -59,8 +36,8 @@ def generate_signals(models, feature_names, data):
         df = data[symbol]
 
         # Preprocess the data and remove the target column
-        X = preprocess_data(df).drop(columns='target')
-        
+        X = preprocess_data(df).drop(columns="target")
+
         # Set the feature names for X to match the feature names used during model training
         X.columns = feature_names[symbol]
 
@@ -68,53 +45,47 @@ def generate_signals(models, feature_names, data):
         next_day_prediction = model.predict(X.iloc[-1].values.reshape(1, -1))[0]
 
         # Implement a trading strategy based on the prediction
-        current_price = df.iloc[-1]['4. close']
-        signal = 'buy' if next_day_prediction > current_price else 'sell'
+        current_price = df.iloc[-1]["4. close"]
+        signal = "buy" if next_day_prediction > current_price else "sell"
         signals[symbol] = signal
 
     return signals
 
 
-
 def execute_trades(signals):
     for symbol, signal in signals.items():
         # Define the trade parameters
-        trade_type = 'buy' if signal == 'buy' else 'sell'
-        qty = 4  # Number of shares to trade
+        trade_type = "buy" if signal == "buy" else "sell"
+        qty = QUANTITY  # Number of shares to trade
 
         # Check if the stock is already in the portfolio
         positions = api.list_positions()
         position = [p for p in positions if p.symbol == symbol]
 
-        if trade_type == 'buy' and not position:
+        if trade_type == "buy" and not position:
             # Submit a buy order
-            print(f"Submitting a buy order for {symbol}")
+            print(f"Submitting a buy order for {symbol}, quantity: {qty}, time_in_force: gtc")
             api.submit_order(
-                symbol=symbol,
-                qty=qty,
-                side='buy',
-                type='market',
-                time_in_force='gtc'
+                symbol=symbol, qty=qty, side="buy", type="market", time_in_force="gtc"
             )
-        elif trade_type == 'sell' and position:
+        elif trade_type == "sell" and position:
             # Submit a sell order
-            print(f"Submitting a sell order for {symbol}")
+            print(f"Submitting a sell order for {symbol}, quantity: {qty}, time_in_force: gtc")
             api.submit_order(
-                symbol=symbol,
-                qty=qty,
-                side='sell',
-                type='market',
-                time_in_force='gtc'
+                symbol=symbol, qty=qty, side="sell", type="market", time_in_force="gtc"
             )
+        else:
+            print(f"No trading done for {symbol}, position was: {position}")
+
 
 # Schedule the bot to run once per day
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Define the list of stock symbols
-    symbols = ['AAPL', 'MSFT', 'GOOGL']
+    symbols = ["AAPL", "MSFT", "GOOGL", "AMC", "COST"]
 
     # Fetch historical data for multiple stocks
-    start = '2018-01-01'
-    end = '2022-01-01'
+    start = "2018-01-01"
+    end = "2022-01-01"
     data = fetch_data_multiple(symbols, start, end)
 
     # Train models for multiple stocks
